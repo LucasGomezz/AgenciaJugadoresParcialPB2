07a655dfa0c4651bdea349169aa08749122f649b
Pablo <pablot.pavan@gmail.com>
2023-05-12 16:33:47 -0300

    prueba

--- test/ar/edu/unlam/pb2/pruebaAgencia.java
+++ test/ar/edu/unlam/pb2/pruebaAgencia.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class pruebaAgencia {
-
+	
 	@Test
 	public void queSePuedaCrearUnaAgenciaConClubYJugadores() {
 //		String nombreAgencia = "Lion Sport";
